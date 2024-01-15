93a1b99551077db7f7c9ab0ae49ae5be21513c7b
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-01-14 19:42:39 -0800

    Add hardcoded messages to user.js

---
+++ lang/messages/en/user.js
@@ -0,0 +1,10 @@
+const MESSAGES = {
+    ASK_AMOUNT: "How many buttons to create?",
+    GO_BTN_INPUT_LABEL: "How many buttons to create?",
+    GO_BTN_VALUE: "Go!",
+    LOSE: "Wrong order!",
+    TITLE: "Lab00 - Button Game",
+    WIN: "Excellent memory!"
+}
+
+export default MESSAGES;
