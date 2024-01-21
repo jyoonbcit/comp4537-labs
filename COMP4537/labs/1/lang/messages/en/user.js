6cda7d1c0fc4d44378978df5217f7d915094831e
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-01-20 21:46:51 -0800

    Add template strings

--- COMP4537/labs/1/lang/messages/en/user.js
+++ COMP4537/labs/1/lang/messages/en/user.js
@@ -1,6 +1,20 @@
 const messages = {
     TITLE: 'Lab 01 - JSON, Object Constructor, localStorage',
-    STORAGE_TIME: 'Stored at: ',
+    STORAGE_TIME: 'Last saved at: ',
+    UPDATE_TIME: 'Last updated at: ',
+    NOTE_DIV: (timestamp, content) => 
+        `<div id='note-row-${timestamp}'> 
+            <textarea id='note-ta-${timestamp}'>${content}</textarea> 
+            <button id='remove${timestamp}'>Remove</button> 
+        </div>`,
+    NOTE_DIV_NO_REMOVE: (timestamp, content) =>
+        `<div id='note-row-${timestamp}'> 
+            <textarea id='note-ta-${timestamp}'>${content}</textarea> 
+        </div>`,
+    SUBMIT_BTN: 'Submit',
+    BACK_BTN: 'Back',
+    WRITER_BTN: 'Writer',
+    READER_BTN: 'Reader'
 }
 
 export default messages
